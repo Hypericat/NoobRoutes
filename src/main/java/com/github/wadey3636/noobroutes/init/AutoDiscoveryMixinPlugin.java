6fb9a4310d9e2b82d8ceca3dcccb9dbf9e8d6598
GitHub Action <action@github.com>
2025-02-27 16:31:15 +0000

    Initialize template repository proper
    Set package name to com.github.wadey3636.noobroutes
    Set project name to NoobRoutes
    Set modid to noobroutes

--- src/main/java/com/github/wadey3636/noobroutes/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/wadey3636/noobroutes/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.wadey3636.noobroutes.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/kotlin/com/github/wadey3636/noobroutes/mixin/MixinGuiMainMenu.java
+++ src/main/kotlin/com/github/wadey3636/noobroutes/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.wadey3636.noobroutes.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
