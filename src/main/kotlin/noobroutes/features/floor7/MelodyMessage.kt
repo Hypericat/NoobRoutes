26e708cb96b0896620e6430fb8688c5e620e7b90
noob <noobusnoobia@gmail.com>
2025-05-08 16:24:51 +0200

    untested mel message
     (cant be worse than the old one anyway)

--- src/main/kotlin/noobroutes/features/floor7/MelodyMessage.kt
+++ src/main/kotlin/noobroutes/features/floor7/MelodyMessage.kt
@@ -1,5 +1,6 @@
 package noobroutes.features.floor7
 
+import net.minecraft.item.Item
 import noobroutes.events.impl.MelodyOpenEvent
 import noobroutes.events.impl.PacketEvent
 import noobroutes.events.impl.S2FPacketSetSlotEvent
@@ -13,7 +14,10 @@ import noobroutes.utils.skyblock.unformattedName
 import net.minecraft.item.ItemStack
 import net.minecraft.network.play.client.C0EPacketClickWindow
 import net.minecraft.network.play.server.S2FPacketSetSlot
+import net.minecraftforge.event.world.WorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import noobroutes.utils.Utils
+import noobroutes.utils.skyblock.sendChatMessage
 import org.lwjgl.input.Keyboard
 
 object MelodyMessage: Module(
@@ -24,56 +28,27 @@ object MelodyMessage: Module(
 ) {
     private val message by StringSetting("message", "", description = "message to send")
 
-    private var melodyWindowId = 0
-    private var melodyClicks = 0
+    private var melodyWindowId = -1
 
     @SubscribeEvent
     fun onMelodyOpen(event: MelodyOpenEvent) {
-        devMessage("melody start detected")
         melodyWindowId = event.packet.windowId
-        melodyClicks = 0
-        sendCommand("pc $message started")
     }
 
     @SubscribeEvent
-    fun onClick(event: PacketEvent.Send) {
-        if (event.packet !is C0EPacketClickWindow || event.packet.windowId != melodyWindowId || event.packet.windowId == 0) return
-        devMessage("melody click detected")
-        melodyClicks++
-        sendCommand("pc $message $melodyClicks/4")
+    fun onS2F(event: S2FPacketSetSlotEvent) {
+        if (event.packet.func_149175_c() != melodyWindowId) return
+        val slots = listOf(16,25,34,43)
+        val index = slots.indexOf(event.packet.func_149173_d())
+        if (index == -1) return
+        val item = event.packet.func_149174_e()
+        val id = Item.getIdFromItem(item.item)
+        if (id != 159 || item.metadata != 5) return
+        sendChatMessage("/pc $message $index /4")
     }
 
-    //@SubscribeEvent
-    fun onSetSlot(event: S2FPacketSetSlotEvent){
-
-        try {
-            val clazz = S2FPacketSetSlot::class.java
-
-
-            val windowIdField = clazz.getDeclaredField("field_149179_a")
-            val slotField = clazz.getDeclaredField("field_149178_b")
-            val stackField = clazz.getDeclaredField("field_149180_c")
-
-            windowIdField.isAccessible = true
-            slotField.isAccessible = true
-            stackField.isAccessible = true
-
-            val windowId = windowIdField.getInt(event.packet)
-            val slot = slotField.getInt(event.packet)
-            val stack = stackField.get(event.packet) as? ItemStack
-
-            if (windowId != melodyWindowId || windowId == 0) {
-                modMessage("Slot $slot was updated with: ${stack.unformattedName}")
-            }
-
-        } catch (e: Exception) {
-            modMessage("broke")
-            e.printStackTrace()
-        }
-
-        modMessage("")
-
-
+    @SubscribeEvent
+    fun onWorldUnload(event: WorldEvent.Unload) {
+        melodyWindowId = -1
     }
-
 }
