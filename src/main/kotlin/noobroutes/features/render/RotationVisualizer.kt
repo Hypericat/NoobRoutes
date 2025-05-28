a962ebab0a1fee1a29e5b4d32d6fc0d5e391d597
noob <noobusnoobia@gmail.com>
2025-05-28 23:35:28 +0200

    rotations dont look like shit anymore

--- src/main/kotlin/noobroutes/features/render/RotationVisualizer.kt
+++ src/main/kotlin/noobroutes/features/render/RotationVisualizer.kt
@@ -1,5 +1,6 @@
 package noobroutes.features.render
 
+import net.minecraft.network.play.client.C03PacketPlayer
 import net.minecraft.network.play.client.C03PacketPlayer.C05PacketPlayerLook
 import net.minecraft.network.play.client.C03PacketPlayer.C06PacketPlayerPosLook
 import net.minecraftforge.client.event.RenderLivingEvent
@@ -8,6 +9,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import noobroutes.events.impl.PacketEvent
 import noobroutes.features.Category
 import noobroutes.features.Module
+import noobroutes.utils.Scheduler
 
 import org.lwjgl.input.Keyboard
 
@@ -18,12 +20,16 @@ object RotationVisualizer: Module(
     description = "shows serverside rotations"
 ) {
     private var lastRot = Pair(0f, 0f)
+    private var almostLastRot = Pair(0f, 0f)
     private var resetPitch: Pair<Float, Float>? = null
+
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     fun onC03(event: PacketEvent.Send) {
-        if (event.packet !is C05PacketPlayerLook && event.packet !is C06PacketPlayerPosLook) return
-        lastRot = Pair(event.packet.yaw, event.packet.pitch)
+        if (event.packet is C05PacketPlayerLook || event.packet is C06PacketPlayerPosLook) {
+            lastRot = Pair(event.packet.yaw, event.packet.pitch)
+            Scheduler.scheduleC03Task { almostLastRot = lastRot }
+        }
     }
 
     @SubscribeEvent
@@ -31,7 +37,7 @@ object RotationVisualizer: Module(
         if (event.entity != mc.thePlayer || mc.thePlayer.ridingEntity != null || mc.currentScreen != null) return
         resetPitch = Pair(event.entity.rotationPitch, event.entity.prevRotationPitch)
         event.entity.rotationPitch = lastRot.second
-        event.entity.prevRotationPitch = lastRot.second
+        event.entity.prevRotationPitch = almostLastRot.second
         event.entity.rotationYawHead = lastRot.first
         event.entity.renderYawOffset = lastRot.first
     }
