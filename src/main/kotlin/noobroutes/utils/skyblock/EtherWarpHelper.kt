f92db5853ce529a3fa7d2e9711d2e7f4723d62da
Wadey3636 <83308534+Wadey3636@users.noreply.github.com>
2025-09-23 22:02:46 -0700

    fixed zpew prediction

--- src/main/kotlin/noobroutes/utils/skyblock/EtherWarpHelper.kt
+++ src/main/kotlin/noobroutes/utils/skyblock/EtherWarpHelper.kt
@@ -1,11 +1,14 @@
 package noobroutes.utils.skyblock
 
+import net.minecraft.block.Block
+import net.minecraft.init.Blocks
 import net.minecraft.util.BlockPos
 import net.minecraft.util.Vec3
 import noobroutes.Core.mc
 import noobroutes.utils.*
 import noobroutes.utils.render.RenderUtils.renderVec
 import noobroutes.utils.skyblock.PlayerUtils.SNEAK_EYE_HEIGHT
+import java.util.BitSet
 import kotlin.math.*
 
 /**
@@ -151,7 +154,14 @@ object EtherWarpHelper {
 
         repeat(1000) {
             val pos = BlockPos(currentPos[0], currentPos[1], currentPos[2])
-            if (getBlockIdAt(pos) != 0) return EtherPos(isValidEtherWarpBlock(pos), pos)
+
+            val chunk = mc.theWorld?.chunkProvider?.provideChunk(pos) ?: return EtherPos.NONE
+            val currentBlock = chunk.getBlock(pos)
+            val currentBlockId = Block.getIdFromBlock(currentBlock)
+            if (!ignoreList.get(currentBlockId)) return EtherPos(
+                isValidEtherWarpBlock(pos),
+                pos
+            )
             if (currentPos.contentEquals(endPos)) return EtherPos.NONE
 
             val minIndex = if (tMax[0] <= tMax[1])
@@ -164,6 +174,21 @@ object EtherWarpHelper {
         }
 
         return EtherPos.NONE
+    }
+
+    private val ignoreList = BitSet(176).apply {
+        arrayOf(
+            Blocks.activator_rail, Blocks.air, Blocks.brown_mushroom, Blocks.carpet,
+            Blocks.carrots, Blocks.cocoa, Blocks.deadbush, Blocks.double_plant, Blocks.fire,
+            Blocks.flower_pot, Blocks.flowing_lava, Blocks.ladder, Blocks.lava, Blocks.lever,
+            Blocks.melon_stem, Blocks.nether_wart, Blocks.piston_extension, Blocks.portal,
+            Blocks.potatoes, Blocks.powered_comparator, Blocks.powered_repeater, Blocks.pumpkin_stem,
+            Blocks.rail, Blocks.red_flower, Blocks.red_mushroom, Blocks.redstone_torch,
+            Blocks.redstone_wire, Blocks.reeds, Blocks.sapling, Blocks.skull, Blocks.snow_layer,
+            Blocks.stone_button, Blocks.tallgrass, Blocks.torch, Blocks.tripwire, Blocks.tripwire_hook,
+            Blocks.unpowered_comparator, Blocks.unpowered_repeater, Blocks.vine, Blocks.water, Blocks.web,
+            Blocks.wheat, Blocks.wooden_button, Blocks.yellow_flower, Blocks.end_portal
+        ).forEach { set(Block.getIdFromBlock(it)) }
     }
     const val DEGREES_TO_RADIAN = Math.PI / 180;
     /**
